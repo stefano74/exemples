1e7c20757ad246f9e40ac77449e8bd662b81d7e8
stefano <s.blasutto@ksd.fr>
2015-02-06 11:27:33 +0100

    ajouter article supprimer article

--- metier_django/serveurweb/urls.py
+++ metier_django/serveurweb/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^articles/$', views.articles.as_view(), name='articles_list'),
     url(r'^articles/(?P<article_id>\d+)/$', views.detail_article, name='detail_article'),
+    url(r'^articles/(?P<article_id>\d+)/del/$', views.supprimer_article, name='supprimer_article'),
+    url(r'^articles/add/$', views.ajouter_article, name='ajouter_article'),
     url(r'^familles/$', views.familles.as_view(), name='familles_list'),
     url(r'^familles/(?P<pk>\d+)/$', views.detail_familles.as_view(), name='familles_detail'),
 )
--- metier_django/serveurweb/views.py
+++ metier_django/serveurweb/views.py
@@ -7,6 +7,8 @@ from django.http.response import Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.forms.models import ModelForm
+from datetime import date
+
 # Create your views here.
 
 def index(request):
@@ -37,9 +39,29 @@ def detail_article(request, article_id):
         if form.is_valid():
             form.save()
         return HttpResponseRedirect(reverse('articles_list'))
-        
-class familles(generic.ListView):
+
+def supprimer_article(request, article_id): 
+    try:
+        article = Articles.objects.get(pk=article_id)
+    except article.DoesNotExist:
+        raise Http404("l'article n'existe pas")
+    article.delete()
+    return HttpResponseRedirect(reverse('articles_list'))
+
+def ajouter_article(request): 
+    article = Articles.objects.create(libelle='', prix=0, date=date.today(), famille=Familles.objects.get(pk=1))
+
+    if request.method == 'GET':
+        form = form_article(instance=article)
+        return render(request, 'serveurweb/article_detail.html', {'form': form, 'article_id': article.id})
     
+    elif request.method == 'POST':
+        form = form_article(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(reverse('articles_list'))
+
+class familles(generic.ListView):
         def get_queryset(self):
             return Familles.objects.order_by('id')
 
