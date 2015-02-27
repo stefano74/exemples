8ca582d87bd14b3ec166b0316425ae3c08968862
stefano <s.blasutto@ksd.fr>
2015-02-27 15:23:11 +0100

    mise en commentaire des middleware protection Car erreur 403 sur PUT POST DELETE

--- metier_django/metier_django/settings.py
+++ metier_django/metier_django/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
