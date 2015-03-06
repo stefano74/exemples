874be089050b309de6a3b569649f8917992d60fe
stefano <s.blasutto@ksd.fr>
2015-03-06 14:53:10 +0100

    suppression DB router -> utilisation defaut DB Ajout login path Ajout static path

--- metier_django/metier_django/settings.py
+++ metier_django/metier_django/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -68,24 +68,28 @@ WSGI_APPLICATION = 'metier_django.wsgi.application'
 
 DATABASES = {
     'default': { # ne sert à rien mais doit être définit : lors de migrations création d'une table django_migrations vide
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/stefano/default',
+        'ENGINE'    : 'django.db.backends.sqlite3',
+        'NAME'      : '/home/stefano/test',
+        'USER'      : '',
+        'PASSWORD'  : '',
+        'HOST'      : 'localhost',
+        'PORT'      : '5432',
     },
-    'postgres': {
-        'ENGINE'    : 'django.db.backends.postgresql_psycopg2',
-        'NAME'      : 'test',
-         'USER'      : '',
-         'PASSWORD'  : '',
-         'HOST'      : 'localhost',
-         'PORT'      : '5432',
-    },
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/stefano/test',
-    }
+#     'postgres': {
+#         'ENGINE'    : 'django.db.backends.postgresql_psycopg2',
+#         'NAME'      : 'test',
+#          'USER'      : '',
+#          'PASSWORD'  : '',
+#          'HOST'      : 'localhost',
+#          'PORT'      : '5432',
+#     },
+#     'sqlite': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': '/home/stefano/test',
+#     }
 }
 
-DATABASE_ROUTERS = ['metier_django.routersDB.SqliteRouter']
+# DATABASE_ROUTERS = ['metier_django.routersDB.SqliteRouter']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -103,7 +107,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # http:/localhost:8000/static/
+STATIC_ROOT = BASE_DIR + '/STATIC/' #répertoire ou les fichiers static se trouve pour la prod
+
 
 # Templates location
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
@@ -122,3 +128,6 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 # wsgi scheme
 os.environ['wsgi.url_scheme'] = 'http'
+
+LOGIN_URL = '/serveurweb/connexion/'
+LOGIN_REDIRECT_URL = '/serveurweb/articles/'
