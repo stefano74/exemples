53375da1d987e8f9c008be2f957c131b601276f8
stefano <s.blasutto@ksd.fr>
2015-03-16 16:45:42 +0100

    ajout doc avec docstrings

--- metier_django/metier_django/urls.py
+++ metier_django/metier_django/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^serveurweb/',    include('serveurweb.urls')),
     url(r'^serveurXMLRPC/', include('serveurXMLRPC.urls')),
     url(r'^serveurREST/',   include('serveurREST.urls')),
+    url(r'^admin/doc/',     include('django.contrib.admindocs.urls')),
     url(r'^admin/',         include(admin.site.urls)),
   )
